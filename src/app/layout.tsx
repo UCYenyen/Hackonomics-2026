deb877507f2e95ab53397438767b191782bdd1fd
Bryan Fernando Dinata <bfernando@student.ciputra.ac.id>
2026-03-23 00:23:42 +0700

    update layout to use navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
+import NavigationBar from "@/components/pages/Layout/NavigationBar";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -31,7 +32,11 @@ export default function RootLayout({
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}<Toaster richColors/></body>
+      <body className="min-h-full flex flex-col">
+        <NavigationBar />
+        {children}
+        <Toaster richColors />
+      </body>
     </html>
   );
 }
